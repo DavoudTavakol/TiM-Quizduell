742bab9e818fe8ac6f837c502c855103f5dbc174
Mohammed <m.bellamine@sentinum.de>
2022-12-03 17:11:11 +0100

    models and repos

--- quiz_api/src/main/kotlin/com/tim/quiz_api/data/HisQuestion.kt
+++ quiz_api/src/main/kotlin/com/tim/quiz_api/data/HisQuestion.kt
@@ -3,7 +3,7 @@ package com.tim.quiz_api.data
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document(value = "Geography")
+@Document(value = "History")
 data class HisQuestion (
     @Id
     val id:String,

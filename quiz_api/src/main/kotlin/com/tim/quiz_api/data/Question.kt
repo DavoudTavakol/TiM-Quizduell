f63d6786adc0f6f6e1ddc17e11305db0046c7880
DavoudTavakol <tavit666@gmail.com>
2022-12-03 16:08:12 +0100

    Update Question.kt
    Formatting

--- quiz_api/src/main/kotlin/com/tim/quiz_api/data/Question.kt
+++ quiz_api/src/main/kotlin/com/tim/quiz_api/data/Question.kt
@@ -3,4 +3,9 @@ package com.tim.quiz_api.data
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "Maths")
-data class Question (val id:String, val question:String, val answer:String, val points:Int)
+data class Question (
+    val id:String,
+    val question:String,
+    val answer:String,
+    val points:Int
+    )

4087408f1e3af363c4168a26f7c50f7f56386ad8
Nico-Mayer <nico.m94@icloud.com>
2022-12-07 16:24:33 +0100

    Admin Board project init

---
+++ quiz_adminpanel/src/lib/Counter.svelte
@@ -0,0 +1,13 @@
+<script>
+	let count = 0;
+
+	function add() {
+		count += 1;
+	}
+</script>
+
+<div>
+	{count}
+</div>
+
+<button on:click={add}>Add</button>
---
+++ quiz_adminpanel/src/routes/login/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	let userName = '';
+	let password = '';
+
+	function handleChange() {
+		console.log('sendToBackend');
+		console.log(userName);
+		console.log(password);
+	}
+</script>
+
+<main>
+	<form action="" on:input={handleChange}>
+		<label for="">Username</label>
+		<input type="text" bind:value={userName} />
+		<label for="">password</label>
+		<input type="password" bind:value={password} />
+		<button>Sign In</button>
+	</form>
+</main>

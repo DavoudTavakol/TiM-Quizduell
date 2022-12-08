cdd3c6fd96a9e2f531b8cc725ef3c6380a87efcf
Nico-Mayer <nico.m94@icloud.com>
2022-12-08 14:10:14 +0100

    side fix

--- quiz_adminpanel/src/routes/+page.svelte
+++ quiz_adminpanel/src/routes/+page.svelte
@@ -1,33 +1,37 @@
 <script>
-	import Counter from '$lib/Counter.svelte';
-	let hover = false;
+	import '../app.css';
+
+	let sidebarElements = [{ title: 'HA' }, { title: 'B' }];
+
+	function addCategory() {
+		sidebarElements = [...sidebarElements, { title: 'CC' }];
+	}
 </script>
 
-<div class="flex flex-col h-screen w-screen">
-	<!-- Link zu Icon Library: https://icones.netlify.app/collection/carbon -->
-	<div class="flex flex-col m-auto ">
-		<section class="flex space-x-8  text-4xl">
-			<div class="animation-speed i-carbon-sun hover:rotate-45" />
-			<div class="i-logos-svelte-icon hover:animate-spin" />
+<main>
+	<div class="h-screen bg-slate-800 w-screen">
+		<div class="flex flex-col h-screen bg-orange-500 w-min content-between justify-between">
 			<div
-				class="smile-emotion"
-				class:laugh-emotion={hover}
-				on:mouseenter={() => (hover = true)}
-				on:mouseleave={() => (hover = false)}
-			/>
-		</section>
-		<Counter />
+				id="sidebar"
+				class="flex flex-col space-y-4 bg-slate-200 flex-1 p-5 float-left overflow-scroll scrollbar-hide"
+			>
+				{#each sidebarElements as element}
+					<div
+						id="sidebarelement"
+						class="rounded-xl flex bg-stone-500 min-h-16 w-16 items-center justify-center"
+					>
+						<span id="category">{element.title}</span>
+					</div>
+				{/each}
+			</div>
+			<div id="addcategory" class="bg-red-500 p-5">
+				<button
+					class="rounded-xl flex bg-stone-500 h-16 w-16 items-center justify-center"
+					on:click={addCategory}
+				>
+					<span id="category" class="font-bold">+</span>
+				</button>
+			</div>
+		</div>
 	</div>
-</div>
-
-<style>
-	.animation-speed {
-		@apply transition-all duration-300;
-	}
-	.smile-emotion {
-		@apply i-twemoji-grinning-face-with-smiling-eyes;
-	}
-	.laugh-emotion {
-		@apply i-twemoji-face-with-tears-of-joy;
-	}
-</style>
+</main>

9caad59889de36d9c3fad7dfa30ae821c452b1a6
RickShaa <r.shaffer@gmx.de>
2022-12-12 11:41:57 +0100

    removed  test methods

--- quiz_api/src/main/kotlin/com/tim/quiz_api/controller/CategoryController.kt
+++ quiz_api/src/main/kotlin/com/tim/quiz_api/controller/CategoryController.kt
@@ -44,37 +44,4 @@ class CategoryController @Autowired constructor(val categoryRepo: CategoryRepo) 
         return ResponseEntity.ok(savedCategory)
     }
 
-    /*@GetMapping("/insert")
-    fun insert(): ResponseEntity<Category> {
-        val testCategoryID = "942f3c05-bba2-47dc-8b37-11dc8847a1b5";
-        //fügt man der Category bei einem save eine ID hinzu welche bereits existiert führt save einen update auf die collection aus, kein insert
-        return ResponseEntity.ok(
-            categoryRepo.save(
-                Category(
-                    "Fragen zum Thema Programmieren in Kotlin",
-                    listOf(Question("Question Title", listOf(), testCategoryID)),
-                    testCategoryID)))
-    }*/
-
-
-   /* @GetMapping("/add/{collectionName}")
-    //durch POST ersetzen
-    fun createCategory(@PathVariable collectionName:String): ResponseEntity<String> {
-        val category = db.listCollectionNames().find{ it==collectionName }
-         if(category != null){
-            return ResponseEntity.badRequest().body("Collection Already Exists")
-        }else{
-            db.createCollection(collectionName)
-            return ResponseEntity.ok("Category created")
-        }
-    }*/
-
-    /*@GetMapping("/createTestDoc")
-    fun createTestDoc(): MutableList<Category> {
-        val categories = listOf<Category>(
-            Category("Mathematik", "Math", listOf("Q1")),
-            Category("Sport", "Sport", listOf("Q2"))
-        )
-        return categoryRepo.saveAll(categories)
-    }*/
 }

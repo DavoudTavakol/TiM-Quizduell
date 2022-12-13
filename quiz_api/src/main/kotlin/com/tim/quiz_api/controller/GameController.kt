0408a3a8203f9f9e92034a6a829c524ad104c14d
Mohammed <m.bellamine@sentinum.de>
2022-12-13 23:40:21 +0100

    get category names

--- quiz_api/src/main/kotlin/com/tim/quiz_api/controller/GameController.kt
+++ quiz_api/src/main/kotlin/com/tim/quiz_api/controller/GameController.kt
@@ -2,11 +2,14 @@ package com.tim.quiz_api.controller
 
 import com.mongodb.client.MongoDatabase
 import com.tim.quiz_api.controller.dto.*
+import com.tim.quiz_api.data.Category
 import com.tim.quiz_api.data.Game
 import com.tim.quiz_api.data.Player
+import com.tim.quiz_api.repository.CategoryRepo
 import com.tim.quiz_api.service.GameService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -17,14 +20,15 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/game")
-class GameController @Autowired constructor(var mongoTemplate: MongoTemplate,private final val gameService: GameService
+class GameController @Autowired constructor(val categoryRepo: CategoryRepo, private final val gameService: GameService
 ) {
-    var db: MongoDatabase = mongoTemplate.db;
 
 
     @PostMapping("/create")
     fun createGame(@RequestBody startRequest: StartRequest ) :
             ResponseEntity<Game> {
+
+
         return ResponseEntity.ok(gameService?.createGame(startRequest))
     }
 
@@ -49,6 +53,18 @@ class GameController @Autowired constructor(var mongoTemplate: MongoTemplate,pri
     fun submitAnswers(@RequestBody request : SubmitRequest ):
             ResponseEntity<Game> {
         return ResponseEntity.ok(gameService?.submitAnswers(request.gameId, request.nickname, request.answers))
+    }
+
+    @GetMapping("/categories")
+    fun getAllCategories(): ResponseEntity<MutableList<String>> {
+        val categories = categoryRepo.findAll() as List<Category>
+        val itr = categories.iterator()
+        var namesList : MutableList<String> = mutableListOf()
+
+        while (itr.hasNext()){
+            namesList.add(itr.next().categoryName)
+        }
+        return ResponseEntity<MutableList<String>>(namesList, HttpStatus.OK)
     }
 
 }

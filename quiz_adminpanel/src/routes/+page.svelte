b52af62f7e6b029a8fbf0d1428bb9ea849453775
TimonLoewl99 <timon.loewl@gmx.de>
2022-12-13 10:09:14 +0100

    UI

--- quiz_adminpanel/src/routes/+page.svelte
+++ quiz_adminpanel/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import '../app.css';
+	import '../lib/Categorys.svelte';
+	import Categorys from '../lib/Categorys.svelte';
 
 	let sidebarElements = [{ title: 'HA' }, { title: 'B' }];
 
@@ -9,29 +11,33 @@
 </script>
 
 <main>
-	<div class="h-screen bg-slate-800 w-screen">
-		<div class="flex flex-col h-screen bg-orange-500 w-min content-between justify-between">
+	<div id='wrapper' class="h-screen bg-gray-700 w-screen flex">
+		<div
+			id="sidebarwrapper"
+			class="flex flex-col h-screen w-min content-between justify-between bg-gray-800 text-white"
+		>
 			<div
 				id="sidebar"
-				class="flex flex-col space-y-4 bg-slate-200 flex-1 p-5 float-left overflow-scroll scrollbar-hide"
+				class="flex flex-col space-y-4 flex-1 p-5 float-left overflow-scroll scrollbar-hide "
 			>
 				{#each sidebarElements as element}
 					<div
 						id="sidebarelement"
-						class="rounded-xl flex bg-stone-500 min-h-16 w-16 items-center justify-center"
+						class="rounded-xl flex bg-gray-700 min-h-16 w-16 items-center justify-center  border-black border-2"
 					>
 						<span id="category">{element.title}</span>
 					</div>
 				{/each}
 			</div>
-			<div id="addcategory" class="bg-red-500 p-5">
+			<div id="addcategory" class="p-5 border-black border-t-4">
 				<button
-					class="rounded-xl flex bg-stone-500 h-16 w-16 items-center justify-center"
+					class="rounded-xl flex bg-gray-600 h-16 w-16 items-center justify-center border-black border-2"
 					on:click={addCategory}
 				>
-					<span id="category" class="font-bold">+</span>
+					<div class="i-twemoji-plus text-3xl" />
 				</button>
 			</div>
 		</div>
+		<Categorys />
 	</div>
 </main>

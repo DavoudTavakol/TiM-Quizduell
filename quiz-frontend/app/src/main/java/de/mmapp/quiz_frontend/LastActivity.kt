ffbdfddad4ec8a8008048b6b0bd03c77da1a463f
santanamartinir79661 <santanamartinir79661@th-nuernberg.de>
2022-12-18 18:22:36 +0100

    edit comments of end screen

--- quiz-frontend/app/src/main/java/de/mmapp/quiz_frontend/LastActivity.kt
+++ quiz-frontend/app/src/main/java/de/mmapp/quiz_frontend/LastActivity.kt
@@ -5,24 +5,31 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class LastActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.highscore_screen)
 
-        //  Gewinner anzeige
+        // display winner
         val winner = findViewById<TextView>(R.id.whoWon)
         winner.setText("'Nickname' hat gewonnen");
+        // TODO show real nickname of winner
+        // who was previously written in main screen
 
-        // Punkte anzeige
+        // display your points
         val points = findViewById<TextView>(R.id.myPoints)
-        points.setText("Punkte: 'Zahl'")
+        points.setText("Deine Punkte: 'Zahl'")
+        // TODO show real achieved points
 
-        // "Highscore" Tabelle anzeige
-        // 3 Spalten:
-        // eine Spalte mit Rang, eine mit den "Nickname" und eine mit die erreichte Punktzahl
-        // ...
+        // display highscore table
+        // 3 columns:
+        // one column with rank, one with the "nickname" and one with the achieved score
+        val rank = findViewById<TextView>(R.id.rank)
+        val name = findViewById<TextView>(R.id.name)
+        val highscore = findViewById<TextView>(R.id.points)
+        // TODO show table with real content in real order
 
         //"Erneut spielen"
         val buttonNewGame = findViewById<Button>(R.id.btn1)
@@ -30,8 +37,9 @@ class LastActivity : AppCompatActivity() {
             //val intent = Intent(this, //Kategorien Activity)
             //startActivity(intent)
         }
+        // TODO create intent to "Kategorie" screen of p1 and to waiting screen for p2
 
-        // "Haputmenue"
+        // "Hauptmenue"
         val buttonMenu = findViewById<Button>(R.id.btn2)
         buttonMenu.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)

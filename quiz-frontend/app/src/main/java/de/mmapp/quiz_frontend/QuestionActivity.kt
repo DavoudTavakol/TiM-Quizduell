c1434377d1fb1e78bbbb6b154d754f884f11b457
Mohammed <m.bellamine@sentinum.de>
2022-12-25 01:13:49 +0100

    question + answers integration

--- quiz-frontend/app/src/main/java/de/mmapp/quiz_frontend/QuestionActivity.kt
+++ quiz-frontend/app/src/main/java/de/mmapp/quiz_frontend/QuestionActivity.kt
@@ -8,24 +8,40 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import de.mmapp.quiz_frontend.models.Answer
 import de.mmapp.quiz_frontend.models.Game
+import de.mmapp.quiz_frontend.models.Question
 
 
 class QuestionActivity : AppCompatActivity() {
+
+    var questionCount : Int = 0
+    var answerList : MutableList<Answer> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.questions_activity)
+        val card = findViewById<CardView>(R.id.card)
+        card.setCardBackgroundColor(Color.rgb(240, 240, 240))
+        card.cardElevation = 10.0F
+
+    }
+
+    override fun onResume() {
+        super.onResume()
 
 
         val game = intent.getParcelableExtra<Game>("game")
         println(game!!.questionList)
 
-        val card = findViewById<CardView>(R.id.card)
-        card.setCardBackgroundColor(Color.rgb(240, 240, 240))
-        card.cardElevation = 10.0F
-        setQuestion()
-        setAnswers()
-        setRound()
+
+        // Init of Question and answers
+        setQuestionNumber()
+        setQuestion(game.questionList.get(0).question)
+        setAnswers(game.questionList)
+
+
+
 
 
         // start progress bar
@@ -34,42 +50,76 @@ class QuestionActivity : AppCompatActivity() {
         val myProgressBar: ProgressBar = findViewById<ProgressBar>(R.id.progressbar)
         myProgressBar.progress = i
 
-        var myCountDownTimer: CountDownTimer = object : CountDownTimer(10000, 1000) {
+        var myCountDownTimer: CountDownTimer = object : CountDownTimer(30000, 1000) {
 
             override fun onTick(millisUntilFinished: Long) {
                 i--
-                myProgressBar.progress = i * (10000 / 1000)
+                myProgressBar.progress = i * (30000 / 1000)
             }
 
             override fun onFinish() {
-                // Zeit abgelaufen
+
+                // TO DO:  Sumbit answers request
+                println(answerList)
             }
         }.start()
 
+
     }
 
-
-    private fun setQuestion() {
+    private fun setQuestion(question : String) {
         // ToDo get the question from API
-        val question = findViewById<TextView>(R.id.question)
-        question.text = "Neue Testfrage"
+        val questionView = findViewById<TextView>(R.id.question)
+        questionView.text = question
     }
 
-    private fun setAnswers() {
+    private fun setAnswers( questionList:  List<Question>) {
         // ToDo get answers from API
         val answer1 = findViewById<Button>(R.id.answer1)
-        answer1.text = "Antwort1"
+        answer1.text = questionList[questionCount].answer[0].answer
+
         val answer2 = findViewById<Button>(R.id.answer2)
-        answer2.text = "Antwort2"
+        answer2.text = questionList[questionCount].answer[1].answer
+
+        // Every Button updates the question and all answers with the next question
+        // To do : Save answers to send them in the timer's onFinish() callback
+
+        answer1.setOnClickListener(){
+
+            // Save Answer
+            answerList.add( questionList[questionCount].answer[0])
+
+            // Update UI
+            setQuestion(questionList[questionCount].question)
+            answer1.text = questionList[questionCount].answer[0].answer
+            answer2.text = questionList[questionCount].answer[1].answer
+
+            questionCount++
+            setQuestionNumber()
+        }
+
+        answer2.setOnClickListener(){
+
+            answerList.add( questionList[questionCount].answer[1])
+
+
+            setQuestion(questionList[questionCount].question)
+            answer1.text = questionList[questionCount].answer[0].answer
+            answer2.text = questionList[questionCount].answer[1].answer
+            questionCount++
+            setQuestionNumber()
+        }
+
         val answer3 = findViewById<Button>(R.id.answer3)
-        answer3.text = "Antwort3"
+        //answer3.text = questionList[0].answer[2].answer
         val answer4 = findViewById<Button>(R.id.answer4)
-        answer4.text = "Antwort4"
+        //answer4.text = questionList[0].answer[3].answer
     }
 
-    private fun setRound() {
+    private fun setQuestionNumber() {
         // ToDo get round
-        val round = findViewById<TextView>(R.id.round)
-        round.text = "Runde 1"
+        val numberView = findViewById<TextView>(R.id.number)
+        var questionNumber : Int = questionCount + 1
+        numberView.text = "Frage Nummer: $questionNumber "
     }
 }

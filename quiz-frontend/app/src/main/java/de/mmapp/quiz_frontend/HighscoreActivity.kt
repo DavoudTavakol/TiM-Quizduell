ed578d6c5c7dc441d6ca80cf8210d7d8f8822cf6
santanamartinir79661 <santanamartinir79661@th-nuernberg.de>
2023-01-08 21:58:33 +0100

    highscore table shows position, nickname and score of topten scores + edit comments + disabled playAgainButton in LastActivty.kt

--- quiz-frontend/app/src/main/java/de/mmapp/quiz_frontend/HighscoreActivity.kt
+++ quiz-frontend/app/src/main/java/de/mmapp/quiz_frontend/HighscoreActivity.kt
@@ -14,16 +14,26 @@ class HighscoreActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.highscore_screen)
 
-        // display highscore table
+        // highscore table
         val topTen = intent.getParcelableArrayListExtra<Score>("topTen")
         val table = findViewById<TextView>(R.id.top10)
-        table.setText(topTen?.toString())
-        // TODO show position, nickname and score only
+        // create String to save the values
+        var text = "\n"
+        // create int to give nr of position
+        var x: Int = 1
+        // get the top ten scores one after the other on separate rows
+        // where [position: nickname: score]
+        for (i in topTen!!) {
+            text += "$x" + " : " + i.nickname + " : " + i.score + "\n"
+            x+=1
+        }
+        // display in screen
+        table.setText(text)
 
-        // "Hauptmenue"
-        // redirect to main screen
+        // "Hauptmenue" button on screen
         val btnMenu = findViewById<Button>(R.id.menu)
         btnMenu.setOnClickListener {
+            // redirect to main screen
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
--- quiz-frontend/app/src/main/java/de/mmapp/quiz_frontend/LastActivity.kt
+++ quiz-frontend/app/src/main/java/de/mmapp/quiz_frontend/LastActivity.kt
@@ -25,47 +25,60 @@ class LastActivity : AppCompatActivity() {
         setContentView(R.layout.end_screen)
 
         // display winner
+        // val nameOfWinner = intent.getExtra(x)
         val winner = findViewById<TextView>(R.id.whoWon)
-        // TODO show winners real nickname
+        // winner.setText(nameOfWinner + " hat gewonnen")
+        // TODO show REAL winners nickname
 
         // display nr of right questions answered
+        // val nrQ = intent.getExtra(y)
         val rightQ = findViewById<TextView>(R.id.nrRightQ)
-        // TODO show real nr of right questions player got
+        // rightQ.setText("Insgesamt " + nrQ.toString() + " von 10 Fragen rightig")
+        // TODO show REAL nr of right questions player got
 
         // display your achieved points
+        // val totalP = intent.getExtra(z)
         val points = findViewById<TextView>(R.id.myPoints)
-        // TODO show real achieved points
+        // points.setText("Punkte: " + totalP.toString())
+        // TODO show REAL achieved points
 
         // display question list
+        // val answeredQ = intent.putExtra(w)
         val list = findViewById<TextView>(R.id.qList)
+        // list.setText(answeredQ.toString())
+        // TODO show REAL full list of answered questions
 
-        //"Erneut spielen"
+        //"Erneut spielen" button on screen
         val btnNewGame = findViewById<Button>(R.id.btn1)
         btnNewGame.setOnClickListener {
+            // redirect to category screen
             val intent = Intent(this, CategoriesActivity::class.java)
             startActivity(intent)
         }
+        // DISABLED AT THE MOMENT
+        btnNewGame.isEnabled = false
         // TODO both players must press "Erneut spielen" to confirm that they are ready.
         //  (Player 1 presses Play again -> wait circle appears until player 2 also presses Play again (the other way round too)).
 
-        // "Hauptmenue"
-        // redirect to main screen
+        // "Hauptmenue" button on screen
         val btnMenu = findViewById<Button>(R.id.btn2)
         btnMenu.setOnClickListener {
+            // redirect to main screen
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        // "Highscore Tabelle anzeigen"
-        // redirect to highscore screen
+        // "Highscore Tabelle anzeigen" button on screen
         val btnHighscore = findViewById<Button>(R.id.btn3)
         btnHighscore.setOnClickListener {
             GlobalScope.launch(Dispatchers.Main) {
                 try {
                     // get highscores from DB
                     val topTen = getHighscoreTable()
+                    // redirect to highscore screen
                     val intent = Intent(this@LastActivity, HighscoreActivity::class.java)
 
+                    // send values to "HighscoreActivity"
                     intent.putExtra("topTen",topTen)
 
                     startActivity(intent)
@@ -77,6 +90,8 @@ class LastActivity : AppCompatActivity() {
         }
     }
 
+    // get highscores from DB
+    // request
     suspend fun getHighscoreTable() : ArrayList<Score> = GlobalScope.async(Dispatchers.IO) {
 
         var topTen : ArrayList<Score> = arrayListOf()

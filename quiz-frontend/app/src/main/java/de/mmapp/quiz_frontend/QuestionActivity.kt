e10d2f2b566c208f00268f51957bff19e669122f
OliverMideck <oliver.mideck@online.de>
2023-01-09 21:24:30 +0100

    adjusted progressbar and question-view

--- quiz-frontend/app/src/main/java/de/mmapp/quiz_frontend/QuestionActivity.kt
+++ quiz-frontend/app/src/main/java/de/mmapp/quiz_frontend/QuestionActivity.kt
@@ -1,28 +1,30 @@
 package de.mmapp.quiz_frontend
 
+import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Button
-import android.widget.ProgressBar
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
-import com.google.android.material.progressindicator.CircularProgressIndicator
-import com.google.android.material.snackbar.Snackbar
 import de.mmapp.quiz_frontend.models.Answer
 import de.mmapp.quiz_frontend.models.Game
 import de.mmapp.quiz_frontend.models.Question
+import kotlinx.coroutines.cancel
 import me.zhanghai.android.materialprogressbar.MaterialProgressBar
-import okio.ByteString.Companion.encodeUtf8
 
 
 class QuestionActivity : AppCompatActivity() {
 
-    private var questionCount : Int = 0
-    private var answerList : MutableList<Answer> = mutableListOf()
+    private var questionCount: Int = 0
+    private var answerList: MutableList<Answer> = mutableListOf()
+    private var points: Int = 0
+
+    // start progress bar
+    var i: Int = 60
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,155 +38,155 @@ class QuestionActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-
         val game = intent.getParcelableExtra<Game>("game")
         println(game!!.questionList)
 
-
         // Init Question and answers
         setQuestionNumber()
-        setQuestion(game.questionList.get(0).question)
+        setQuestion(game.questionList[0].question)
         setAnswers(game.questionList)
 
 
-
-       // 863116
-
-        // start progress bar
-
-        var i : Int = 59
         val myProgressBar: MaterialProgressBar = findViewById<MaterialProgressBar>(R.id.progressbar)
         val timeView = findViewById<TextView>(R.id.gameTimer)
-        myProgressBar.progress = 99
+        val mCountDownTimer: CountDownTimer
+        myProgressBar.progress = 100
 
-       // val colList = ColorStateList.valueOf(170)
-
-       // myProgressBar.progressTintList = colList
-
-
-
-        var isFirst : Boolean = true
-        myProgressBar.setOnClickListener {
-
-            if (isFirst){
-                var myCountDownTimer: CountDownTimer =  object : CountDownTimer(10000, 1000) {
-
-                    override fun onTick(millisUntilFinished: Long) {
-                    }
-
-                    override fun onFinish() {
-                        var x = myProgressBar.progress
-                        timeView.text = x.toString()
-                        myProgressBar.progress = 100
-
-                    }
-                }.start()
-
-                isFirst = false;
-            } else {
-                println("clicked")
-                myProgressBar.progress += 1
-
-            }
-
-
-        }
-
-
-        422195
-
-
-        var myCountDownTimer: CountDownTimer = object : CountDownTimer(60000, 1000) {
-
+        mCountDownTimer = object : CountDownTimer(60000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-
+                println("this is a tick ${myProgressBar.progress}")
                 i--
-                if(myProgressBar.progress > 4 && i >= 10){
-                    myProgressBar.progress -= 2
-                    timeView.text = ":$i"
-
-                } else if (i in 0..10){
-                    timeView.text = "$i"
-                    timeView.setTextColor(Color.RED)
-                    myProgressBar.supportProgressTintList = ColorStateList.valueOf(Color.RED)
-                } else {
-                    timeView.text = ""
-                    myProgressBar.progress -= 2
-                    myProgressBar.progress -= 2
-
-
-                }
-                //myProgressBar.progress = i * (30000 / 1000)
-                //println("this is a tick ${myProgressBar.progress}")
-
+                timeView.text = i.toString()
+                myProgressBar.progress = (i * (5.0 / 3.0)).toInt()
             }
 
             override fun onFinish() {
-
-                // TO DO:  Sumbit answers request
-                println(answerList)
+                // ToDo submit all answers -> API
             }
-        }.start()
-        myCountDownTimer
+        }
 
-
+        mCountDownTimer.start()
     }
 
-    private fun setQuestion(question : String) {
-        // ToDo get the question from API
+    private fun setQuestion(question: String) {
         val questionView = findViewById<TextView>(R.id.question)
         questionView.text = question
     }
 
-    private fun setAnswers( questionList:  List<Question>) {
-        // ToDo get answers from API
+    private fun setAnswers(questionList: List<Question>) {
+
         val answer1 = findViewById<Button>(R.id.answer1)
         answer1.text = questionList[questionCount].answer[0].answer
 
         val answer2 = findViewById<Button>(R.id.answer2)
         answer2.text = questionList[questionCount].answer[1].answer
 
+        val answer3 = findViewById<Button>(R.id.answer3)
+        answer3.text = questionList[questionCount].answer[2].answer
+
+        val answer4 = findViewById<Button>(R.id.answer4)
+        answer4.text = questionList[questionCount].answer[3].answer
+
         // Every Button updates the question and all answers with the next question
-        // To do : Save answers to send them in the timer's onFinish() callback
-
-        answer1.setOnClickListener(){
-
+        // ToDo : Save answers to send them in the timer's onFinish() callback
+        answer1.setOnClickListener() {
+/*
+            if (questionCount == 9) {
+                println("DEBUUUG")
+                val intent =
+                    Intent(this@QuestionActivity, HighscoreActivity::class.java)
+                intent.putExtra("points", points)
+                startActivity(intent)
+            }
+*/
             // Save Answer
-            answerList.add( questionList[questionCount].answer[0])
+            answerList.add(questionList[questionCount].answer[0])
+
+            // Update the points of the player
+            updatePoints(0, questionList)
 
             // Update UI
             questionCount++
             setQuestionNumber()
 
+            // Load next question
             setQuestion(questionList[questionCount].question)
+            // Load next answers
             answer1.text = questionList[questionCount].answer[0].answer
             answer2.text = questionList[questionCount].answer[1].answer
-
-
+            answer3.text = questionList[questionCount].answer[2].answer
+            answer4.text = questionList[questionCount].answer[3].answer
         }
 
-        answer2.setOnClickListener(){
+        answer2.setOnClickListener() {
+            // Save answer
+            answerList.add(questionList[questionCount].answer[1])
 
-            answerList.add( questionList[questionCount].answer[1])
+            // Update the points of the player
+            updatePoints(1, questionList)
 
             questionCount++
             setQuestionNumber()
 
+            // Load next question
             setQuestion(questionList[questionCount].question)
+            // Load next answers
             answer1.text = questionList[questionCount].answer[0].answer
             answer2.text = questionList[questionCount].answer[1].answer
-
+            answer3.text = questionList[questionCount].answer[2].answer
+            answer4.text = questionList[questionCount].answer[3].answer
         }
 
-        val answer3 = findViewById<Button>(R.id.answer3)
-        //answer3.text = questionList[0].answer[2].answer
-        val answer4 = findViewById<Button>(R.id.answer4)
-        //answer4.text = questionList[0].answer[3].answer
+        answer3.setOnClickListener() {
+            // Save answer
+            answerList.add(questionList[questionCount].answer[2])
+
+            // Update the points of the player
+            updatePoints(2, questionList)
+
+            questionCount++
+            setQuestionNumber()
+
+            // Load next question
+            setQuestion(questionList[questionCount].question)
+            // Load next answers
+            answer1.text = questionList[questionCount].answer[0].answer
+            answer2.text = questionList[questionCount].answer[1].answer
+            answer3.text = questionList[questionCount].answer[2].answer
+            answer4.text = questionList[questionCount].answer[3].answer
+        }
+
+        answer4.setOnClickListener() {
+            // Save answer
+            answerList.add(questionList[questionCount].answer[3])
+
+            // Update the points of the player
+            updatePoints(3, questionList)
+
+            questionCount++
+            setQuestionNumber()
+
+            // Load next question
+            setQuestion(questionList[questionCount].question)
+            // Load next answers
+            answer1.text = questionList[questionCount].answer[0].answer
+            answer2.text = questionList[questionCount].answer[1].answer
+            answer3.text = questionList[questionCount].answer[2].answer
+            answer4.text = questionList[questionCount].answer[3].answer
+        }
+    }
+
+    private fun updatePoints(numberOfAnswer: Int, questionList: List<Question>) {
+        val isAnswerCorrect = questionList[questionCount].answer[numberOfAnswer].isAnswerCorrect
+        if (isAnswerCorrect) {
+            points++
+            print("Points of the player: " + points.toString() + "\n")
+        }
     }
 
     private fun setQuestionNumber() {
         val numberView = findViewById<TextView>(R.id.number)
-        var questionNumber : Int = questionCount + 1
+        val questionNumber: Int = questionCount + 1
         numberView.text = "Frage Nummer: $questionNumber "
     }
 }
